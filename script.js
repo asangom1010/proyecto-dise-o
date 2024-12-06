6d7e82de248d2205e456ee227d7e4884586fecf2
asangom1010 <asangom1010@g.educaand.es>
2024-12-06 21:53:48 +0100

    se han añadido paginas nuevas y modificado index, torneos, y cursos
    se han creado pagina para las inscripciones, se ha actualizado el logo, se ha aplicado boton para volver al inicio de la pagina, se ha creado la estructura de cursos y torneos, se han añadido imagenes ...

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const backToTopButton = document.getElementById("backToTop");
+
+    // Mostrar u ocultar el botón según el scroll
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) { // Mostrar cuando scroll > 300px
+            backToTopButton.style.display = "block";
+        } else {
+            backToTopButton.style.display = "none";
+        }
+    });
+
+    // Volver al principio del sitio al hacer clic
+    backToTopButton.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth", // Desplazamiento suave
+        });
+    });
+});
